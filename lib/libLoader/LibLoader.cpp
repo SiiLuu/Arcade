0f2164234fff2c16d1f97b49025923bf18b45499
Julien Salies <J.Salies@localhost.localdomain>
2020-03-19 05:26:45 -0400

    [ADDED] libLoader // file to change the lib

---
+++ lib/libLoader/LibLoader.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** Arcade
+** File description:
+** lib loader class
+*/
+
+#include "LibLoader.hpp"
+
+LibLoader::LibLoader()
+{
+}
+
+LibLoader::~LibLoader()
+{
+}
---
+++ lib/libLoader/LibLoader.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2020
+** Arcade
+** File description:
+** Lib loader class
+*/
+
+#ifndef LIBLOADER_HPP_
+#define LIBLOADER_HPP_
+
+class LibLoader {
+    public:
+        LibLoader();
+        ~LibLoader();
+
+        void load();
+        void unLoad();
+
+    protected:
+    private:
+};
+
+#endif /* !LIBLOADER_HPP_ */
